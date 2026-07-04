41453a109d2b3966783639404709b9d71b0ca754
muuhesham <muhammedhesham488@gmail.com>
2026-07-04 17:04:17 +0300

    add loggers to console & files - update configuration the ts-types in project

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,7 +1,8 @@
 import cors from "cors";
+import { FRONTEND_URL } from "./env.js";
 
 const corsOptions = {
-  origin: "*",
+  origin: FRONTEND_URL,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
---
+++ src/config/logger.ts
@@ -0,0 +1,25 @@
+import morgan from "morgan";
+import fs from "fs";
+import path from "path";
+
+const __dirname = import.meta.dirname;
+
+const logDirectory = path.join(__dirname, "../logs");
+
+if(!fs.existsSync(logDirectory)) {
+    fs.mkdirSync(logDirectory);
+}
+
+morgan.token("user_ID", (req) => {
+    return req.user ? req.user._id : "Guest";
+});
+
+const customFormat =
+  ':remote-addr - :user-id [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"';
+
+const accessLogStream = fs.createWriteStream(path.join(logDirectory, "access.log"), { flags: "a" });
+
+const fileLogger = morgan(customFormat, { stream: accessLogStream });
+const consoleLogger = morgan("dev");
+
+export default [fileLogger, consoleLogger];

4192961e400af027791d66f04a31c5ca8363fbb0
muuhesham <muhammedhesham488@gmail.com>
2026-07-06 07:31:46 +0300

    review the code of routes and controllers & make changes for last structure

--- src/controllers/subjectController.ts
+++ src/controllers/subjectController.ts
@@ -5,23 +5,23 @@ import { sendResponse } from "../utils/sendResponse.js";
 
 const subjectController = {
   getSubjects: asyncHandler(async (req: Request, res: Response) => {
-    const subjects = await subjectService.getSubjectsByUser(req.user!._id);
+    const subjects = await subjectService.getSubjectsByUser(req.user._id);
     return sendResponse(res, 200, true, undefined, subjects);
   }),
 
   addSubject: asyncHandler(async (req: Request, res: Response) => {
-    const subject = await subjectService.addSubject(req.user!._id, req.body);
-    return sendResponse(res, 201, true, undefined, subject);
+    const subject = await subjectService.addSubject(req.user._id, req.body);
+    return sendResponse(res, 201, true, "Subject Added Successfully", subject);
   }),
 
   deleteSubject: asyncHandler(async (req: Request, res: Response) => {
-    await subjectService.deleteSubject(req.params.id! as string, req.user!._id);
-    return sendResponse(res, 200, true);
+    await subjectService.deleteSubject(req.params.id as string, req.user._id);
+    return sendResponse(res, 200, true, "Subject Deleted Successfully");
   }),
 
   updateSubject: asyncHandler(async (req: Request, res: Response) => {
-    const subject = await subjectService.updateSubject(req.params.id! as string, req.user!._id, req.body);
-    return sendResponse(res, 200, true, undefined, subject);
+    const subject = await subjectService.updateSubject(req.params.id as string, req.user._id, req.body);
+    return sendResponse(res, 200, true, "Subject Updated Successfully", subject);
   }),
 };
 
--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,42 +1,68 @@
 import { Request, Response } from "express";
 import userService from "../services/userService.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
+import { sendResponse } from "../utils/sendResponse.js";
 
 const userController = {
-  userProfile: asyncHandler(async (req: Request, res: Response) => {
-    const userId = req.user!._id;
+  getUserProfile: asyncHandler(async (req: Request, res: Response) => {
+    const userId = req.user._id;
     const { user } = await userService.userProfile({ userId });
 
-    return res.status(200).json({ success: true, user });
+    return sendResponse(res, 200, true, undefined, user);
   }),
 
   changeUserName: asyncHandler(async (req: Request, res: Response) => {
-    const userId = req.user!._id;
+    const userId = req.user._id;
     const name = req.body.name;
     const { user } = await userService.changeUserName({ userId, name });
 
-    return res.status(200).json({
-      success: true,
-      message: "Username updated successfully",
-      user,
-    });
+    return sendResponse(res, 200, true, "Username updated successfully", user);
   }),
 
   changePassword: asyncHandler(async (req: Request, res: Response) => {
-    const userId = req.user!._id;
+    const userId = req.user._id;
     const { currentPassword, newPassword } = req.body;
-    await userService.changePassword({ userId, currentPassword, newPassword });
+    const {user, token} = await userService.changePassword({ userId, currentPassword, newPassword });
 
-    return res
-      .status(200)
-      .json({ success: true, message: "Password updated successfully" });
+    return sendResponse(res, 200, true, "Password updated successfully", {user, token});
   }),
 
   deleteProfile: asyncHandler(async (req: Request, res: Response) => {
-    const userId = req.user!._id;
+    const userId = req.user._id;
     await userService.deleteProfile({ userId });
 
-    return res.status(204).json({});
+    return sendResponse(res, 204, true, "Account deleted Successfully");
+  }),
+
+  changeDailyHours: asyncHandler(async (req: Request, res: Response) => {
+    const userId = req.user._id;
+    const { newDailyHours } = req.body;
+    const { user } = await userService.changeDailyHours({
+      newDailyHours,
+      userId,
+    });
+
+    return sendResponse(
+      res,
+      200,
+      true,
+      "DailyHours updated Successfully",
+      user,
+    );
+  }),
+
+  changeEmail: asyncHandler(async (req: Request, res: Response) => {
+    const userId = req.user._id;
+    const { newEmail } = req.body;
+    const { user } = await userService.changeEmail({ newEmail, userId });
+
+    return sendResponse(
+      res,
+      200,
+      true,
+      "Email updated Successfully",
+      user,
+    );
   }),
 };
 

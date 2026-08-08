90fe49cd28f65e6bb9e09ec0d48e51284a328d26
muuhesham <muhammedhesham488@gmail.com>
2026-08-08 08:00:26 +0300

    edit env config

--- src/config/env.ts
+++ src/config/env.ts
@@ -12,20 +12,16 @@ function requireEnv(name: string): string {
   return value;
 }
 
-function optionalEnv(name: string): string | undefined {
-  return process.env[name];
-}
-
 export const DB_URL: string = requireEnv("DB_URL");
 export const PORT: number = requireEnv("PORT")
   ? parseInt(requireEnv("PORT"), 10)
   : 3000;
 export const JWT_KEY: string = requireEnv("JWT_KEY");
 export const FRONTEND_URL: string = requireEnv("FRONTEND_URL");
-export const EMAIL_HOST: string | undefined = optionalEnv("EMAIL_HOST");
-export const EMAIL_PORT: number | undefined = optionalEnv("EMAIL_PORT")
-  ? parseInt(optionalEnv("EMAIL_PORT") as string, 10)
-  : undefined;
-export const EMAIL_USER: string | undefined = optionalEnv("EMAIL_USER");
-export const EMAIL_PASS: string | undefined = optionalEnv("EMAIL_PASS");
-export const EMAIL_FROM: string | undefined = optionalEnv("EMAIL_FROM");
+export const EMAIL_HOST: string = requireEnv("EMAIL_HOST");
+export const EMAIL_PORT: number = process.env.EMAIL_PORT
+  ? parseInt(process.env.EMAIL_PORT, 10)
+  : 465;
+export const EMAIL_USER: string = requireEnv("EMAIL_USER");
+export const EMAIL_PASS: string = requireEnv("EMAIL_PASS");
+export const EMAIL_FROM: string = requireEnv("EMAIL_FROM");
